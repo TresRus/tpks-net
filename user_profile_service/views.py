1c543b9fcafd80c89b9d4afdb997c35a701e3791
bohulenkov14 <bohulenkov14@gmail.com>
2012-10-06 23:51:51 +0400

    Added some test for interaction of socail with UserService

--- user_profile_service/views.py
+++ user_profile_service/views.py
@@ -1,1 +1,7 @@
 # Create your views here.
+from django.http import HttpResponse
+import json
+
+def detail(request, user_id):
+    response_data = {user_id: "That's his data"}
+    return HttpResponse(json.dump(response_data), mimetype='application/json')
