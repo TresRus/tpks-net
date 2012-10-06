6babe9edff694f166182cfe33b55b6ddc549b40a
bohulenkov14 <bohulenkov14@gmail.com>
2012-10-06 23:22:58 +0400

    added model fields

--- user_profile_service/models.py
+++ user_profile_service/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    login = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    nqme = models.CharField(max_length=30)
+    surname = models.CharField(max_length=30)
+    patronymic = models.CharField(max_length=30)
+    post = models.CharField(max_length=60)
+    home_phonenumber = models.CharField(max_length=30)
+    mobile_phonenumber = models.CharField(max_length=30)
+    workspace_address = models.CharField(max_length=200)
+
+
