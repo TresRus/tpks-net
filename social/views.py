294be6dc9cd4ba996848a547e1c35ddd96ca2351
bohulenkov14 <bohulenkov14@gmail.com>
2012-10-07 01:03:04 +0400

    added dummy interaction between social network facade and user profile service via json

--- social/views.py
+++ social/views.py
@@ -1,1 +1,11 @@
+from django.http import  HttpRequest, HttpResponse
+import user_profile_service.views
+
+
 # Create your views here.
+
+def login(request):
+    req = HttpRequest()
+    resp = user_profile_service.views.detail(req, 0)
+    return HttpResponse(resp, mimetype='application/json')
+
--- user_profile_service/views.py
+++ user_profile_service/views.py
@@ -4,4 +4,4 @@ import json
 
 def detail(request, user_id):
     response_data = {user_id: "That's his data"}
-    return HttpResponse(json.dump(response_data), mimetype='application/json')
+    return HttpResponse(json.dumps(response_data), mimetype='application/json')
