38e8c8a7b1b69ca0ed50696595c9b0c0021d0e53
Artem <hat@mephist.ru>
2012-12-02 00:56:02 +0400

    Flush 'em all!
    [-] All python crap
    [*] README.md now contains SOME info

--- social/urls.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'bohulenkov14'
-from django.conf.urls import patterns, include, url
-
-
-urlpatterns = patterns('social.views',
-    url(r'^$', 'login_user'),
-)
--- user_profile_service/urls.py
+++
@@ -1,6 +0,0 @@
-__author__ = 'bohulenkov14'
-from django.conf.urls import patterns, include, url
-
-urlpatterns = patterns('user_profile_service.views',
-    url(r'^(?P<user_id>\d+)/$', 'detail'),
-)
